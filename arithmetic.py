8a6bb48caf96d29b2e763cafc6ed4055b03907e4
rdraschwandtner <54244077+rdraschwandtner@users.noreply.github.com>
2019-09-04 20:51:50 +0200

    Implemented results from ./experiments/experiment-map-vs-list-comp-vs-lambda-list-comp.ipynb

--- arithmetic.py
+++ arithmetic.py
@@ -1,69 +1,68 @@
 import operator
-import functools
-import numbers
 
-def listoperation_wrapper(lhs, rhs, op):
-    # bind operator-function to function call. Functions are not iterable, so map() would throw an error.
-    return list(map(functools.partial(listoperation,op=op), lhs, rhs)) # https://stackoverflow.com/questions/10314859/applying-map-for-partial-argument
-    
-def listoperation(lhs, rhs, op):
-    if isinstance(lhs, list) == True:
-        return listoperation_wrapper(lhs, rhs, op)
-    elif (isinstance(lhs, numbers.Number) & isinstance(rhs, numbers.Number)): 
-        return op(lhs, rhs) # lhs and rhs are scalars
-    else:
-        raise Exception('Structure not OK. lhs: ' + str(lhs) + ' rhs: ' + str(rhs))
+# See ./experiments/experiment-map-vs-list-comp-vs-lambda-list-comp.ipynb
+# https://stackoverflow.com/a/57615641/11963667
+def element_wise_lstcompfct(lhs, rhs, f):
+    return [element_wise_lstcompfct(i, j, f) if type(i) == list and type(j) == list else f(i, j) for i, j in zip(lhs, rhs)]
 
 
-def my_arbitrarylistsum(lhs, rhs):
-    return listoperation_wrapper(lhs, rhs, operator.add)
+def listsum(lhs, rhs):
+    return element_wise_lstcompfct(lhs, rhs, operator.add)
 
-def my_arbitrarylistsub(lhs, rhs):
-    return listoperation_wrapper(lhs, rhs, operator.sub)
+def listsub(lhs, rhs):
+    return element_wise_lstcompfct(lhs, rhs, operator.sub)
 
-def my_arbitrarylistmul(lhs, rhs):
-    return listoperation_wrapper(lhs, rhs, operator.mul)
+def listmul(lhs, rhs):
+    return element_wise_lstcompfct(lhs, rhs, operator.mul)
 
-def my_arbitrarylistdiv(lhs, rhs):
-    return listoperation_wrapper(lhs, rhs, operator.truediv)
+def listdiv(lhs, rhs):
+    return element_wise_lstcompfct(lhs, rhs, operator.truediv)
+
+
 
 
 def main():
-    # sum
+	# sum
+	a = [1,2,3,4,[5,6,7,8]]
+	b = [10,20,30,40,[50,60,70,80]]
+	c = listsum(a,b)
+	c
+    
+    
 	e = [1,2,3,4,[5,6,7,8]]
-	my_arbitrarylistsum(e, e)
+	listsum(e, e)
 	
 	f = [1,[2,3], 4, [5,6,7], [[8], [9,10]]]
 	g = [10,[20,30], 40, [50,60,70], [[80], [90,100]]]
-	my_arbitrarylistsum(f, g)
+	listsum(f, g)
     
-	my_arbitrarylistsub(e, e)
-	my_arbitrarylistsub(f, g)
-	my_arbitrarylistsub(g, f)
+	listsub(e, e)
+	listsub(f, g)
+	listsub(g, f)
     
-	my_arbitrarylistmul(e,e)
-	my_arbitrarylistmul(f, g)
+	listmul(e,e)
+	listmul(f, g)
     
-	my_arbitrarylistdiv(e,e)
-	my_arbitrarylistdiv(f, g) 
+	listdiv(e,e)
+	listdiv(f, g) 
 
 	# negative tests
 	h = [1,2,3,4,[5,'a',7,8]]
 	try:
-		my_arbitrarylistsum(h, h) # should raise exception
+		listsum(h, h) # should raise exception
 	except Exception as error:
 		print(error)
 		pass
     
 	try:
-		my_arbitrarylistsum(e, h) # should raise exception
+		listsum(e, h) # should raise exception
 	except Exception as error:
 		print(error)
 		pass
     
 	i = [1,2,3,4,[5,0,7,8]]    
 	try:
-		my_arbitrarylistdiv(e, i) # should raise exception
+		listsum(e, i) # should raise exception
 	except Exception as error:
 		print(error)
 		pass
